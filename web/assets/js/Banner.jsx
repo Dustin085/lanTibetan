6d31ee00df5042ed4a0e6d6fdfa4ff435725d4b6
Dustin085 <dustin830927@gmail.com>
2024-09-10 11:30:01 +0800

    banner-basic-finish

---
+++ web/assets/js/Banner.jsx
@@ -0,0 +1,13 @@
+function Banner() {
+    return <>
+        <section id="banner">
+            <h1 className="title-box">
+                <div className="title-tc">荏芊堪卓</div>
+                <div className="title-tibetan">རིན་ཆེན་མཁའ་འགྲོ།</div>
+            </h1>
+            <div className="slide-box">
+                <div className="slide"><img src="./assets/images/banner/banner1.jpg" alt="首頁輪播圖1" /></div>
+            </div>
+        </section>
+    </>
+}
