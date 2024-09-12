8ee3be47ea20225fe4638dbc8ee422b023d71520
Dustin085 <dustin830927@gmail.com>
2024-09-12 16:52:48 +0800

    0912

--- web/assets/js/LanArticle.jsx
+++ web/assets/js/LanArticle.jsx
@@ -1,7 +1,10 @@
 function LanArticle(props) {
     const { title = "關於荏芊堪卓",
         subtitle = "理念闡述或標語理念闡述標語理念闡述或標語",
-        content = "文字簡介文字簡介文字簡介<br/>文字簡介文字簡介文字<br/>簡介文字簡介文字簡介文字<br/>簡介文字簡介文字簡介<br/>文字簡介文字簡介文字簡介文字簡介"
+        content = "文字簡介文字簡介文字簡介<br/>文字簡介文字簡介文字<br/>簡介文字簡介文字簡介文字<br/>簡介文字簡介文字簡介<br/>文字簡介文字簡介文字簡介文字簡介",
+        link = "#",
+        noMoreBtn = false,
+        moreBtnIconContent = "→"
     } = props;
     return <>
         <article className="lan-article">
@@ -22,17 +25,20 @@ function LanArticle(props) {
                         })
                     }
                 </p>
-                <ViewMoreBtn url="#" />
+                {
+                    noMoreBtn ? <></> : <ViewMoreBtn url={link} moreBtnIconContent={moreBtnIconContent} />
+                }
+
             </div>
         </article>
     </>
 }
 
-function ViewMoreBtn({ url }) {
+function ViewMoreBtn({ url, moreBtnIconContent }) {
     return <>
         <a href={url} className="view-more-btn">
             <p className="text">view more</p>
-            <div className="icon">→</div>
+            <div className="icon">{moreBtnIconContent}</div>
         </a>
     </>
 }
