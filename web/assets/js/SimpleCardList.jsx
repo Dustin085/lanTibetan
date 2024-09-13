2bf82908118c02987a09fe6a969168398160f2b0
Dustin085 <dustin830927@gmail.com>
2024-09-13 08:21:02 +0800

    almost-finish

---
+++ web/assets/js/SimpleCardList.jsx
@@ -0,0 +1,42 @@
+function SimpleCardList(props) {
+    const { listData = [] } = props;
+
+
+    return <>
+        <ul className="simple-card-list">
+            {
+                listData.map((item, index) => {
+                    const {
+                        link,
+                        imgUrl,
+                        title,
+                    } = item;
+                    return (
+                        <li className="simple-card-item" key={index}>
+                            <SimpleCardListItem
+                                link={link}
+                                imgUrl={imgUrl}
+                                title={title}
+                            />
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    </>
+
+    function SimpleCardListItem(props) {
+        const {
+            link = "#",
+            imgUrl = "./assets/images/picture/buddhist-text3.jpg",
+            title = "藏地語文",
+        } = props;
+        return <>
+            <a href={link} className="simple-card">
+                <picture className="picture-masked"><img src={imgUrl} alt="" /></picture>
+                <h3 className="title">{title}</h3>
+                <div class="split-line-row"></div>
+            </a>
+        </>
+    }
+}
